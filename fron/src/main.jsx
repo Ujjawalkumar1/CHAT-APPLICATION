4adc50262d48e1c45e32e2b93d6d21f7297fa522
Ujjawal Kumar <ujjawalkumar787898@gmail.com>
2025-07-19 06:59:00 +0530

    full login support code done

--- fron/src/main.jsx
+++ fron/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import store from './redux/store';  // Make sure your store is correctly set up
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <>
+        <App />
+        <Toaster />
+      </>
+    </Provider>
+  </StrictMode>
+);
